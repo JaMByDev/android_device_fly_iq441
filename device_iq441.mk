39faecec59c0b850194299f7f6fb5042b8f98a86
JaMByDev <jambydevs@gmail.com>
2016-05-19 08:13:30 +0300

    Add board class ril

--- device_iq441.mk
+++ device_iq441.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
      	FmRadio \
      	libfmjni \
-	libfmmt6620 \
+	    libfmmt6620 \
         libfmmt6628 \
         libfmmt6627 \
         libfmmt6630 \
